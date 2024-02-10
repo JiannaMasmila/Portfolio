d2769e94bbb117073e27d7cd4da43d0cebe242b4
ripudamansinghthind <ripudamansinghthind@gmail.com>
2024-02-10 14:03:26 -0800

    fixed landing page - syncing with main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import Image from 'next/image'
 import '/src/app/globals.css'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center p-24 bg-white">
-      <div className="max-w-5xl w-full flex flex-col items-center justify-center">
+      <div className="snap-y snap-mandatory max-w-5xl w-full flex flex-col items-center justify-center">
         <p className="fixed left-0 top-0 flex w-full justify-center pb-6 pt-8 lg:static lg:w-auto">
           <code className="animate-marquee infinite text-custom-size font-bold whitespace-nowrap mr-40" style={{ fontFamily: 'PanchangBold', color: '#131313' }}>
             JIANNA MASMILA
