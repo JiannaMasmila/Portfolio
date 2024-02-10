a663f9f1bb7b108d9569e3f2059104daef199e51
ripudamansinghthind <ripudamansinghthind@gmail.com>
2024-02-10 12:05:14 -0800

    Initialized

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+import '/src/app/globals.css'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center p-24 bg-white">
+      <div className="max-w-5xl w-full flex flex-col items-center justify-center">
+        <p className="fixed left-0 top-0 flex w-full justify-center pb-6 pt-8 lg:static lg:w-auto">
+          <code className="font-mono font-bold text-9xl whitespace-nowrap" style={{ fontFamily: 'PanchangBold', color: '#131313' }}>
+            JIANNA MASMILA
+          </code>
+        </p>
+      </div>
+    </main>
+  )
+}
---
+++ src/app/users/new/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewUserPage = () => {
+    return (
+        <div>
+            <p>NewUserPage</p>
+        </div>
+    )
+}
+
+export default NewUserPage
