d3bcbe93a7ecbc23d16b19e01ca07f62dae80e64
ripudamansinghthind <ripudamansinghthind@gmail.com>
2024-02-10 13:38:16 -0800

    Landing Page created.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,16 @@ export default function Home() {
     <main className="flex min-h-screen items-center justify-center p-24 bg-white">
       <div className="max-w-5xl w-full flex flex-col items-center justify-center">
         <p className="fixed left-0 top-0 flex w-full justify-center pb-6 pt-8 lg:static lg:w-auto">
-          <code className="font-mono font-bold text-9xl whitespace-nowrap" style={{ fontFamily: 'PanchangBold', color: '#131313' }}>
+          <code className="animate-marquee infinite text-custom-size font-bold whitespace-nowrap mr-40" style={{ fontFamily: 'PanchangBold', color: '#131313' }}>
+            JIANNA MASMILA
+          </code>
+          <code className="animate-marquee infinite text-custom-size font-bold whitespace-nowrap mr-40" style={{ fontFamily: 'PanchangBold', color: '#131313' }}>
+            JIANNA MASMILA
+          </code>
+          <code className="animate-marquee infinite text-custom-size font-bold whitespace-nowrap mr-40" style={{ fontFamily: 'PanchangBold', color: '#131313' }}>
+            JIANNA MASMILA
+          </code>
+          <code className="animate-marquee infinite text-custom-size font-bold whitespace-nowrap mr-40" style={{ fontFamily: 'PanchangBold', color: '#131313' }}>
             JIANNA MASMILA
           </code>
         </p>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,7 @@
 import type { Config } from 'tailwindcss'
 
+const colors = require('tailwindcss/colors')
+
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -10,11 +12,26 @@ const config: Config = {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      animation: {
+        marquee: 'marquee 25s linear infinite',
+        marquee2: 'marquee2 25s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+        marquee2: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0%)' },
+        },
       },
     },
   },
+  variants: {},
   plugins: [],
 }
+
 export default config
